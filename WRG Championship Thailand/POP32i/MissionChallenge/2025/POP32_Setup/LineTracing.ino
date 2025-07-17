be54a63fdc51e2b4fba325e00988e6b83ef0e1e9
ysx <tunpisittune@gmail.com>
2025-07-17 07:26:22 +0700

    Update LineTracing.ino

--- WRG Championship Thailand/POP32i/MissionChallenge/2025/POP32_Setup/LineTracing.ino
+++ WRG Championship Thailand/POP32i/MissionChallenge/2025/POP32_Setup/LineTracing.ino
@@ -4,8 +4,8 @@ void LineTracing() {
   while (true) {
     ShowADC();
 
-    sleep(2000);
-    break;
+    // sleep(2000);
+    // break;
   }
   oled.clear();
   oled.text(3, 4, "Press A to go to the next step");
